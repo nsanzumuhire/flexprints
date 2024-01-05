bced39d8baf3c2a318e6078d4c15b9ad79dcecd5
Nsanzumuhire Daniel <nsanzudaniel59@gmail.com>
2024-01-05 14:21:31 +0200

    restructured & renamed the project apps

---
+++ apps/flexprints-payment-services/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'flexprints-payment-services',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/flexprints-payment-services',
+};
